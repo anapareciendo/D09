7a140516ed243ddfea1326387977248ad69ecbbb
Carmen <Student@10.0.2.15>
2017-03-21 19:07:03 +0100

    Algunas queries dashboard comentadas

--- Acme-CnG/src/main/java/repositories/AdministratorRepository.java
+++ Acme-CnG/src/main/java/repositories/AdministratorRepository.java
@@ -12,4 +12,44 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 	
 	@Query("select a from Administrator a where a.userAccount.id = ?1")
 	Administrator findByUserAccountId(int id);
+	
+	//Level C
+	
+	//Ratio of offers versus requests
+	//Select o from Offer o;
+	//Select r from Request r;
+	
+	//Average number of offers and request per customer.
+	//Select avg(c.offers.size) from Customer c;
+	//Select avg(c.requests.size) from Customer c;
+	
+	//Average number of applications per offer or request.
+	//Select avg(a.demand.size) from Application a;
+	
+	//The customer who has more applications accepted
+	//select distinct(d.customer) from Demand d join d.application a where d.application.size = (select max(d.application.size) from Demand d join d.application a where a.status = 1) AND r.status = 1
+	
+	//The customer who has more applications denied.
+	//Probar la de arriba
+	
+	//Level B
+	
+	//Average number of comments per actor
+	//Select avg(a.comments.size) from Actor a;
+	
+	
+	//Average number of comments per offer
+	//Average number of comments per request
+	//Probar la primera
+	
+	// Average number of comments posted by administrators and customer
+	//Select avg(a.postComments) from Actor a;
+	
+	//The actors who have posted ±10% the average number of comments per actor
+	
+	//Level A
+	
+	
+	
+	
 }
