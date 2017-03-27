26d9ec53a70c1ae1892373b18f3ad6d3839a1c69
anapareciendo <ana.apariciora@gmail.com>
2017-03-27 19:51:21 +0200

    Eficiencia de las queries

--- Acme-CnG/src/main/java/domain/Application.java
+++ Acme-CnG/src/main/java/domain/Application.java
@@ -4,12 +4,15 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = { @Index(columnList = "status")})
 public class Application extends DomainEntity {
 
 	//----------------------Attributes-------------------------
--- Acme-CnG/src/main/java/domain/Demand.java
+++ Acme-CnG/src/main/java/domain/Demand.java
@@ -7,8 +7,10 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -20,6 +22,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = { @Index(columnList = "type, banned, title, description")})
 public class Demand extends Commentable {
 
 	//----------------------Attributes-------------------------
